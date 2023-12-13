5a5ad997a4206e74b8725109fff71bd0f025f0ad
supminn <sansup49@gmail.com>
2023-12-13 20:39:00 +0530

    sync supabse stripe tables, copy subscription into lib/supabase schema

--- src/lib/supabase/schema.ts
+++ src/lib/supabase/schema.ts
@@ -1,4 +1,14 @@
-import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
+import {
+  boolean,
+  integer,
+  jsonb,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
+import { prices, subscriptionStatus } from "../../../migrations/schema";
 
 export const workspaces = pgTable("workspaces", {
   id: uuid("id").defaultRandom().primaryKey().notNull(),
@@ -28,9 +38,11 @@ export const folders = pgTable("folders", {
   data: text("data"),
   inTrash: text("in_trash"),
   bannerUrl: text("banner_url"),
-  workspaceId: uuid("workspace_id").references(() => workspaces.id, {
-    onDelete: "cascade",
-  }),
+  workspaceId: uuid("workspace_id")
+    .notNull()
+    .references(() => workspaces.id, {
+      onDelete: "cascade",
+    }),
 });
 
 export const files = pgTable("files", {
@@ -45,10 +57,61 @@ export const files = pgTable("files", {
   data: text("data"),
   inTrash: text("in_trash"),
   bannerUrl: text("banner_url"),
-  workspaceId: uuid("workspace_id").references(() => workspaces.id, {
-    onDelete: "cascade",
-  }),
-  folderId: uuid("folder_id").references(() => folders.id, {
-    onDelete: "cascade",
-  }),
+  workspaceId: uuid("workspace_id")
+    .notNull()
+    .references(() => workspaces.id, {
+      onDelete: "cascade",
+    }),
+  folderId: uuid("folder_id")
+    .notNull()
+    .references(() => folders.id, {
+      onDelete: "cascade",
+    }),
+});
+
+export const subscriptions = pgTable("subscriptions", {
+  id: text("id").primaryKey().notNull(),
+  userId: uuid("user_id").notNull(),
+  status: subscriptionStatus("status"),
+  metadata: jsonb("metadata"),
+  priceId: text("price_id")
+    .references(() => prices.id)
+    .references(() => prices.id),
+  quantity: integer("quantity"),
+  cancelAtPeriodEnd: boolean("cancel_at_period_end"),
+  created: timestamp("created", { withTimezone: true, mode: "string" })
+    .default(sql`now()`)
+    .notNull(),
+  currentPeriodStart: timestamp("current_period_start", {
+    withTimezone: true,
+    mode: "string",
+  })
+    .default(sql`now()`)
+    .notNull(),
+  currentPeriodEnd: timestamp("current_period_end", {
+    withTimezone: true,
+    mode: "string",
+  })
+    .default(sql`now()`)
+    .notNull(),
+  endedAt: timestamp("ended_at", {
+    withTimezone: true,
+    mode: "string",
+  }).default(sql`now()`),
+  cancelAt: timestamp("cancel_at", {
+    withTimezone: true,
+    mode: "string",
+  }).default(sql`now()`),
+  canceledAt: timestamp("canceled_at", {
+    withTimezone: true,
+    mode: "string",
+  }).default(sql`now()`),
+  trialStart: timestamp("trial_start", {
+    withTimezone: true,
+    mode: "string",
+  }).default(sql`now()`),
+  trialEnd: timestamp("trial_end", {
+    withTimezone: true,
+    mode: "string",
+  }).default(sql`now()`),
 });
