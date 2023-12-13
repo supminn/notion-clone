ba3d36ce206dcf4b2377553d6cf9a398b15c40d7
Supriya M <supriya.minnasandram@ck12.org>
2023-12-13 20:14:46 +0530

    setup drizzle, supabase; connect db and set migration

---
+++ src/lib/supabase/schema.ts
@@ -0,0 +1,17 @@
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+export const workspaces = pgTable("workspaces", {
+  id: uuid("id").defaultRandom().primaryKey().notNull(),
+  createdAt: timestamp("created_at", {
+    withTimezone: true,
+    mode: "string",
+  }),
+  workspaceOwner: uuid("workspace_owner").notNull(), // workspaceOwner is used locally in this app; workspace_owner is the column name within the database table
+  title: text("title").notNull(),
+  iconId: text("icon_id").notNull(),
+  data: text("data"),
+  inTrash: text("in_trash"),
+  logo: text("logo"),
+  bannerUrl: text("banner_url"),
+});
+// This is called pattern recognition
