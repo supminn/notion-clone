db9dfc923759c88c9dcacffcf3fd92f2dc521711
supminn <sansup49@gmail.com>
2023-12-14 09:43:00 +0530

    fix: use client and import type mistake

--- src/lib/providers/next-theme-provider.tsx
+++ src/lib/providers/next-theme-provider.tsx
@@ -1,9 +1,10 @@
-"use-client";
+'use client';
 
+import * as React from 'react';
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { ThemeProviderProps } from "next-themes/dist/types";
+import { type ThemeProviderProps } from "next-themes/dist/types";
 
-export default function ThemeProvider({
+export function ThemeProvider({
   children,
   ...props
 }: ThemeProviderProps) {
