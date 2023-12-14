819129aafee850c345a1da5973a77fb9e4cbc741
Supriya M <supriya.minnasandram@ck12.org>
2023-12-14 22:12:52 +0530

    landing page title section with image banner

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import TitleSection from "@/components/landingPage/TitleSection";
+import Image from "next/image";
+import React from "react";
+import Banner from "../../../public/appBanner.png";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <section>
+      <div className="overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center">
+        <TitleSection
+          pill="✨ Your Workspace, Perfected"
+          title="All-In-One Collaboration and Productivity Platform"
+        />
+        <div className="bg-white p-[2px] mt-6 rounded-xl bg-gradient-to-r from-primary to-brand-primaryBlue sm:w-[300px]">
+          {/* <Button
+            variant="btn-secondary"
+            className=" w-full
+            rounded-[10px]
+            p-6
+            text-2xl
+            bg-background
+          "
+          >
+            Get Cypress Free
+          </Button> */}
+        </div>
+        <div className="md:mt-[-90px] sm:w-full w-[750px] flex justify-center items-center mt-[-40px] relative sm:ml-0 ml-[-50px]">
+          <Image src={Banner} alt="Application Banner" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default HomePage
+export default HomePage;
