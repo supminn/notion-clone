006967cd5e0367795d7263bf53c127e5bbe7a262
Supriya M <supriya.minnasandram@ck12.org>
2023-12-15 19:50:55 +0530

    adding calendar image, and section title for testimonials

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -2,6 +2,7 @@ import TitleSection from "@/components/landingPage/TitleSection";
 import Image from "next/image";
 import React from "react";
 import Banner from "../../../public/appBanner.png";
+import Cal from "../../../public/cal.png";
 import { Button } from "@/components/ui/Button";
 import { CLIENTS } from "@/lib/contants";
 
@@ -10,14 +11,14 @@ const HomePage = () => {
     <>
       <section
         className="overflow-hidden 
-      px-4 
-      sm:px-6 
-      mt-10 
-      sm:flex 
-      sm:flex-col 
-      gap-4 
-      md:justify-center 
-      md:items-center"
+        px-4 
+        sm:px-6 
+        mt-10 
+        sm:flex 
+        sm:flex-col 
+        gap-4 
+        md:justify-center 
+        md:items-center"
       >
         <TitleSection
           pill="✨ Your Workspace, Perfected"
@@ -25,13 +26,13 @@ const HomePage = () => {
         />
         <div
           className="bg-white 
-        p-[2px] 
-        mt-6 
-        rounded-xl 
-        bg-gradient-to-r 
-        from-primary 
-        to-brand-primaryBlue 
-        sm:w-[300px]"
+          p-[2px] 
+          mt-6 
+          rounded-xl 
+          bg-gradient-to-r 
+          from-primary 
+          to-brand-primaryBlue 
+          sm:w-[300px]"
         >
           <Button
             variant="btn-secondary"
@@ -47,62 +48,63 @@ const HomePage = () => {
         </div>
         <div
           className="md:mt-[-90px] 
-        sm:w-full 
-        w-[750px] 
-        flex 
-        justify-center 
-        items-center 
-        mt-[-40px] 
-        relative 
-        sm:ml-0 
-        ml-[-50px]"
+          sm:w-full 
+          w-[750px] 
+          flex 
+          justify-center 
+          items-center 
+          mt-[-40px] 
+          relative 
+          sm:ml-0 
+          ml-[-50px]"
         >
           <Image src={Banner} alt="Application Banner" />
           <div
             className="absolute 
-          bottom-0 
-          top-[50%] 
-          bg-gradient-to-t 
-          dark:from-background 
-          left-0 
-          right-0 
-          z-10"
+            bottom-0 
+            top-[50%] 
+            bg-gradient-to-t 
+            dark:from-background 
+            left-0 
+            right-0 
+            z-10"
           />
         </div>
       </section>
       <section className="relative">
         <div
           className="overflow-hidden 
-        flex 
-        after:content[''] 
-        after:dark:from-brand-dark 
-        after:to-transparent 
-        after:from-background 
-        after:bg-gradient-to-l 
-        after:right-0 
-        after:top-0 
-        after:bottom-0 
-        after:w-20 
-        after:absolute
-        after:z-10 
-        before:content[''] 
-        before:dark:from-brand-dark 
-        before:to-transparent 
-        before:from-background 
-        before:bg-gradient-to-r 
-        before:left-0 
-        before:top-0 
-        before:bottom-0 
-        before:w-20
-        before:z-10 
-        before:absolute"
+          flex 
+          after:content[''] 
+          after:dark:from-brand-dark 
+          after:to-transparent 
+          after:from-background 
+          after:bg-gradient-to-l 
+          after:right-0 
+          after:top-0 
+          after:bottom-0 
+          after:w-20 
+          after:absolute
+          after:z-10 
+          before:content[''] 
+          before:dark:from-brand-dark 
+          before:to-transparent 
+          before:from-background 
+          before:bg-gradient-to-r 
+          before:left-0 
+          before:top-0 
+          before:bottom-0 
+          before:w-20
+          before:z-10 
+          before:absolute"
         >
+          {/* Repeat the logo list twice */}
           {[...Array(2)].map((arr) => (
             <div
               key={arr}
               className="flex 
-            flex-nowrap 
-            animate-slide"
+              flex-nowrap 
+              animate-slide"
             >
               {CLIENTS.map((client) => (
                 <div
@@ -123,6 +125,64 @@ const HomePage = () => {
               ))}
             </div>
           ))}
+        </div>
+      </section>
+      <section
+        className="px-4
+        flex
+        justify-center
+        items-center
+        flex-col
+        relative"
+      >
+        {/* <div
+          className="w-[30%]
+          blur-[120px]
+          rounded-full
+          h-32
+          absolute
+        bg-brand-primaryPurple/50
+          -z-10
+          top-22"
+        /> */}
+        <TitleSection
+          title="Keep track of your meetings all in one place"
+          subHeading="Capture your ideas, thoughts, and meeting notes in a structured and organized manner."
+          pill="Features"
+        />
+        <div
+          className="mt-10
+          max-w-[450px]
+          justify-center
+          items-center
+          relative
+          sm:ml-0
+          rounded-2xl
+          border-8
+          border-washed-blue-300
+          border-opacity-10"
+        >
+          <Image src={Cal} alt="Banner" className="rounded-2xl" />
+        </div>
+      </section>
+      <section className="relative">
+        {/* <div
+          className="w-full
+          blur-[120px]
+          rounded-full
+          h-32
+          absolute
+        bg-brand-primaryPurple/50
+          -z-10
+          top-56"
+        /> */}
+        <div className="mt-20 px-4 sm:px-6 flex flex-col overflow-x-hidden overflow-visible">
+          <TitleSection
+            title="Trusted by all"
+            subHeading="Join thousands of satisfied users who rely on our platform for their 
+            personal and professional productivity needs."
+            pill="Testimonials"
+          />
         </div>
       </section>
     </>
