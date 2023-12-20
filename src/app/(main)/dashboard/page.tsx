215defdba2e0fba8b9ef2d5ef33f0352b89cf3dd
supminn <sansup49@gmail.com>
2023-12-20 19:16:00 +0530

    Added user and subscription from supabase

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import db from "@/lib/supabase/db";
 import { redirect } from "next/navigation";
 import DashboardSetup from "@/components/dashboardSetup/DashboardSetup";
+import { getUserSubscriptionStatus } from "@/lib/supabase/queries";
 
 const DashboardPage = async () => {
   const supabase = createServerComponentClient({ cookies });
@@ -15,11 +16,16 @@ const DashboardPage = async () => {
   const workspace = await db.query.workspaces.findFirst({
     where: (workspace, { eq }) => eq(workspace.workspaceOwner, user.id),
   });
-
+  const { data: subscription, error: subscriptionError } =
+    await getUserSubscriptionStatus(user.id);
+  if (subscriptionError) return;
   if (!workspace)
     return (
       <div className="bg-background h-screen w-screen flex justify-center items-center">
-        <DashboardSetup user={user} subscription={{}}></DashboardSetup>
+        <DashboardSetup
+          user={user}
+          subscription={subscription}
+        ></DashboardSetup>
       </div>
     );
 
---
+++ src/components/global/EmojiPicker.tsx
@@ -0,0 +1,30 @@
+import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { Popover } from "../ui/popover";
+import { PopoverContent, PopoverTrigger } from "@radix-ui/react-popover";
+
+interface EmojiPickerProps {
+  children: React.ReactNode;
+  getValue?: (emoji: string) => void;
+}
+
+const EmojiPicker: React.FC<EmojiPickerProps> = ({ children, getValue }) => {
+  const router = useRouter();
+  const Picker = dynamic(() => import("emoji-picker-react"));
+  const onClick = (selectedEmoji: any) => {
+    if (getValue) getValue(selectedEmoji.emoji);
+  };
+  return (
+    <div className="flex items-center">
+      <Popover>
+        <PopoverTrigger className="cursor-pointer">{children}</PopoverTrigger>
+        <PopoverContent className="p-0 border-none">
+          <Picker onEmojiClick={onClick} />
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+};
+
+export default EmojiPicker;
