7e832be54c0dc3132423ec9ef11cf7d5dc898e95
supminn <sansup49@gmail.com>
2023-12-20 19:26:00 +0530

    wip: coded from video

--- src/components/dashboardSetup/DashboardSetup.tsx
+++ src/components/dashboardSetup/DashboardSetup.tsx
@@ -1,17 +1,39 @@
 "use client";
 import { AuthUser } from "@supabase/supabase-js";
-import React from "react";
+import React, { useState } from "react";
 import { Card, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import EmojiPicker from "../global/EmojiPicker";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Subscription } from "@/lib/supabase/supabase.types";
+import { CreateWorkspaceFormSchema } from "@/lib/types";
+import { z } from "zod";
+import { randomUUID } from "crypto";
 
 interface DashboardSetupProps {
   user: AuthUser;
-  subscription: {} | null;
+  subscription: Subscription | null;
 }
 
 const DashboardSetup: React.FC<DashboardSetupProps> = ({
   user,
   subscription,
 }) => {
+  const [selectedEmoji, setSelectedEmoji] = useState("💼");
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isSubmitting: isLoading, errors },
+  } = useForm<z.infer<typeof CreateWorkspaceFormSchema>>({
+    mode: "onChange",
+    defaultValues: {
+      logo: "",
+      workspaceName: "",
+    },
+  });
+
   return (
     <Card className="w-[800px] h-screen sm:h-auto">
       <CardHeader>
@@ -21,7 +43,53 @@ const DashboardSetup: React.FC<DashboardSetupProps> = ({
           collaborators later from the workspace settings tab.
         </CardDescription>
         <form onSubmit={() => {}}>
-          <div className="flex flex-col gap-4"></div>
+          <div className="flex flex-col gap-4">
+            <div className="flex items-center gap-4">
+              <div className="text-5xl">
+                <EmojiPicker getValue={(emoji) => setSelectedEmoji(emoji)}>
+                  {selectedEmoji}
+                </EmojiPicker>
+              </div>
+              <div className="w-full">
+                <Label
+                  htmlFor="workspaceName"
+                  className="text-sm text-muted-foreground"
+                >
+                  Name
+                </Label>
+                <Input
+                  id="workspaceName"
+                  type="text"
+                  placeholder="Workspace Name"
+                  disabled={isLoading}
+                  {...register("workspaceName", {
+                    required: "Workspace name is required",
+                  })}
+                />
+                <small className="text-red-600">
+                  {errors.workspaceName?.message?.toString()}
+                </small>
+              </div>
+            </div>
+            <div className="w-full">
+              <Label htmlFor="logo" className="text-sm text-muted-foreground">
+                Workspace Logo
+              </Label>
+              <Input
+                id="logo"
+                type="file"
+                accept="image/*"
+                placeholder="Workspace Name"
+                disabled={isLoading || subscription?.status !== "active"}
+                {...register("logo", {
+                  required: "Workspace logo is required",
+                })}
+              />
+              <small className="text-red-600">
+                {errors.logo?.message?.toString()}
+              </small>
+            </div>
+          </div>
         </form>
       </CardHeader>
     </Card>
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,3 +4,13 @@ export const FormSchema = z.object({
   email: z.string().describe("Email").email({ message: "Invalid Email" }),
   password: z.string().describe("Password").min(1, "Password is required"),
 });
+
+export const CreateWorkspaceFormSchema = z.object({
+  workspaceName: z
+    .string()
+    .describe("workspaceName")
+    .min(1, "Workspace name must be min of 1 character"),
+  logo: z.any(),
+});
+
+// using z.any for logo since it would cause an issue on the server side otherwise
