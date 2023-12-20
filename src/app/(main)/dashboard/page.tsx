0c799c455b14cfa7e339e2ff56bf6d091a9df2c1
supminn <sansup49@gmail.com>
2023-12-20 18:41:00 +0530

    working well until now. DashboardSetup template created

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { cookies } from "next/headers";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import db from "@/lib/supabase/db";
+import { redirect } from "next/navigation";
+import DashboardSetup from "@/components/dashboardSetup/DashboardSetup";
 
-const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+const DashboardPage = async () => {
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) return;
+  const workspace = await db.query.workspaces.findFirst({
+    where: (workspace, { eq }) => eq(workspace.workspaceOwner, user.id),
+  });
+
+  if (!workspace)
+    return (
+      <div className="bg-background h-screen w-screen flex justify-center items-center">
+        <DashboardSetup user={user} subscription={{}}></DashboardSetup>
+      </div>
+    );
+
+  redirect(`/dashboard/${workspace.id}`);
 };
 
 export default DashboardPage;
