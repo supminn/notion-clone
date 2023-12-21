e429414e2c54d69643c7b34434e513c264883d78
supminn <sansup49@gmail.com>
2023-12-21 18:58:24 +0530

    wip: getting collaborators

---
+++ src/components/global/CollaboratorSearch.tsx
@@ -0,0 +1,113 @@
+"use client";
+import { useSupabaseUser } from "@/lib/providers/supabase-user-provider";
+import { User } from "@/lib/supabase/supabase.types";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
+import { Search } from "lucide-react";
+import { Input } from "../ui/input";
+import { ScrollArea } from "../ui/scroll-area";
+import { Avatar, AvatarFallback } from "../ui/avatar";
+import { AvatarImage } from "@radix-ui/react-avatar";
+import { Button } from "../ui/button";
+
+interface CollaboratorSearchProps {
+  existingCollaborators: User[] | [];
+  getCollaborator: (collaborator: User) => void;
+  children: React.ReactNode;
+}
+const CollaboratorSearch: React.FC<CollaboratorSearchProps> = ({
+  existingCollaborators,
+  getCollaborator,
+  children,
+}) => {
+  const [searchResults, setSearchResults] = useState<User[] | []>([]);
+  const timerRef = useRef<ReturnType<typeof setTimeout>>();
+  const { user } = useSupabaseUser();
+
+  useEffect(() => {
+    return () => {
+      if (timerRef.current) clearTimeout(timerRef.current);
+    };
+  }, []);
+
+  const onChangeHandler = () => {};
+  const addCollaborator = (user: User) => {
+    getCollaborator(user);
+  };
+
+  return (
+    <Sheet>
+      <SheetTrigger className="w-full">{children}</SheetTrigger>
+      <SheetContent className="w-[400px] sm:w-[540px]">
+        <SheetHeader>
+          <SheetTitle>Search Collaborator</SheetTitle>
+          <SheetDescription>
+            <p className="text-sm text-muted-foreground">
+              You can also remove collaborators after adding them from the
+              settings tab.
+            </p>
+          </SheetDescription>
+        </SheetHeader>
+        <div className="flex justify-center items-center gap-2 mt-2">
+          <Search />
+          <Input
+            name="name"
+            className="dark:bg-background"
+            placeholder="Email"
+            onChange={onChangeHandler}
+          />
+        </div>
+        <ScrollArea className="mt-6 overflow-y w-full rounded-md">
+          {searchResults
+            .filter(
+              (result) =>
+                !existingCollaborators.some(
+                  (existingCollaborator) =>
+                    existingCollaborator.id === result.id
+                )
+            )
+            .filter((result) => result.id !== user?.id)
+            .map((user) => (
+              <div
+                className="p-4 flex justify-between items-center"
+                key={user.id}
+              >
+                <div className="flex gap-4 items-center">
+                  <Avatar className="w-8 h-8">
+                    {/* TODO: Fetch avatars from storage and display it here. */}
+                    <AvatarImage src="/avatars/7.png" />
+                    <AvatarFallback>AV</AvatarFallback>
+                  </Avatar>
+                  <div
+                    className="text-sm
+                      gap-2
+                      overflow-hidden
+                      overflow-ellipsis
+                      w-[180px]
+                      text-muted-foreground"
+                  >
+                    {user.email}
+                  </div>
+                </div>
+                <Button
+                  variant="secondary"
+                  onClick={() => addCollaborator(user)}
+                >
+                  Add
+                </Button>
+              </div>
+            ))}
+        </ScrollArea>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default CollaboratorSearch;
--- src/components/global/WorkspaceCreator.tsx
+++ src/components/global/WorkspaceCreator.tsx
@@ -1,14 +1,32 @@
 "use client";
 import { useSupabaseUser } from "@/lib/providers/supabase-user-provider";
-import { User } from "@/lib/supabase/supabase.types";
+import { User, Workspace } from "@/lib/supabase/supabase.types";
 import React, { useState } from "react";
 import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { Lock, Plus, Share } from "lucide-react";
+import { Button } from "../ui/button";
+import { v4 } from "uuid";
+import { addCollaborators, createWorkspace } from "@/lib/supabase/queries";
+import { useAppState } from "@/lib/providers/state-provider";
+import { useRouter } from "next/navigation";
+import CollaboratorSearch from "./CollaboratorSearch";
 
 const WorkspaceCreator = () => {
+  const router = useRouter();
   const { user } = useSupabaseUser();
   const [permissions, setPermissions] = useState("private");
   const [title, setTitle] = useState("");
   const [collaborators, setCollaborators] = useState<User[]>([]);
+  const { dispatch } = useAppState();
 
   const addCollaborator = (user: User) => {
     setCollaborators((prev) => [...prev, user]);
@@ -20,13 +38,110 @@ const WorkspaceCreator = () => {
     );
   };
 
+  const createItem = async () => {
+    const uuid = v4();
+    if (user?.id) {
+      const newWorkspace: Workspace = {
+        data: null,
+        createdAt: new Date().toISOString(),
+        iconId: "💼",
+        id: uuid,
+        inTrash: "",
+        title,
+        workspaceOwner: user.id,
+        logo: null,
+        bannerUrl: "",
+      };
+      if (permissions === "private") {
+        await createWorkspace(newWorkspace);
+        dispatch({
+          type: "ADD_WORKSPACE",
+          payload: { ...newWorkspace, folders: [] },
+        });
+      }
+      if (permissions === "shared") {
+        await createWorkspace(newWorkspace);
+        await addCollaborators(collaborators, uuid);
+        router.refresh();
+      }
+    }
+  };
   return (
-    <div className="flex gap-4 flex-col">
+    <div className="flex gap-4 flex-col mt-4">
       <div>
         <Label htmlFor="name" className="text-sm text-muted-foreground">
           Name
         </Label>
+        <div className="flex justify-center items-center gap-2">
+          <Input
+            name="name"
+            value={title}
+            placeholder="Workspace Name"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
       </div>
+      <>
+        <Label htmlFor="permissions" className="text-sm text-muted-foreground">
+          Permissions
+        </Label>
+        <Select
+          onValueChange={(value) => setPermissions(value)}
+          defaultValue={permissions}
+        >
+          <SelectTrigger className="w-full h-26 -mt-3">
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="private">
+                <div className="p-2 flex gap-4 justify-center items-center">
+                  <Lock />
+                  <article className="text-left flex flex-col">
+                    <span>Private</span>
+                    <p>
+                      Your workspace is private to you. You can choose to share
+                      it later.
+                    </p>
+                  </article>
+                </div>
+              </SelectItem>
+              <SelectItem value="shared">
+                <div className="p-2 flex gap-4 justify-center items-center">
+                  <Share />
+                  <article className="text-left flex flex-col">
+                    <span>Shared</span>
+                    <p>You can invite collaborators</p>
+                  </article>
+                </div>
+              </SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </>
+      {permissions === "shared" && (
+        <div>
+          <CollaboratorSearch
+            existingCollaborators={collaborators}
+            getCollaborator={(user) => addCollaborator(user)}
+          >
+            <Button type="button" className="text-sm mt-4">
+              <Plus />
+              Add Collaborators
+            </Button>
+          </CollaboratorSearch>
+        </div>
+      )}
+      <Button
+        type="button"
+        disabled={
+          !title || (permissions === "shared" && collaborators.length === 0)
+        }
+        variant="secondary"
+        onClick={createItem}
+      >
+        Create
+      </Button>
     </div>
   );
 };
